20e77f52e9541bfc97f7b0bc81d4a15959af9fa5
Facundo Solari <facusolari9@gmail.com>
2026-01-07 23:21:25 -0300

    Cambios API

--- Back/Proyecto-Braian/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Back/Proyecto-Braian/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eacfe6e7100ec93b11397a283a1bd98f8bc6022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a451cc72aff0bc8697f63aa7a098e72e7e0c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
