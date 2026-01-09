7898d0cd9e8d2c135d6be52745037cde4247e2e1
Facundo Solari <facusolari9@gmail.com>
2026-01-09 18:46:55 -0300

    prueba

--- Back/Proyecto-Braian/ProyectoBraian/Program.cs
+++ Back/Proyecto-Braian/ProyectoBraian/Program.cs
@@ -28,6 +28,9 @@ using Infrastructure.Extensions;
     builder.Services.AddHttpContextAccessor();
     builder.Services.AddJwtAuthentication(builder.Configuration);
 
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");  
     var app = builder.Build();
 
     // ----------------------------
